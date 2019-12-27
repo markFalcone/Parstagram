4f92291153912d678ff494543938f4cdc79085d8
markFalcone <falconemark16@yahoo.com>
2019-12-26 22:57:43 -0800

    First Commit - implemented home screen ** not formatted** - set up parse and able to create an account and sign in

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Mark Falcone on 12/26/19.
+//  Copyright © 2019 Mark Falcone. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func inSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+                }else{
+                    print("error: \(error?.localizedDescription)")
+                }
+                
+            }
+        }
+        
+        
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.signUpInBackground { (success, error) in
+            if success{
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+            }else{
+                print("error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
