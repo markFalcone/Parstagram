14147321b3b818453d35a876a9ff5c14696efa0f
markFalcone <falconemark16@yahoo.com>
2019-12-27 17:04:55 -0800

    able to see last posts - name and comment but not image

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Mark Falcone on 12/27/19.
+//  Copyright © 2019 Mark Falcone. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
