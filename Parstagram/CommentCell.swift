c0c686b3ac63b33eda72baedab539f04198f6d7a
markFalcone <falconemark16@yahoo.com>
2020-01-03 19:23:34 -0800

    adding comment cell

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Mark Falcone on 1/3/20.
+//  Copyright © 2020 Mark Falcone. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
